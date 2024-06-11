1ad147c1f94f5bd610c93117510a489a190f49fc
Carlo <carlo.panzi@gmail.com>
2024-06-11 15:35:39 +0200

    slides: better

--- m1-corsi-base-spec/src/js/tippy.js
+++ m1-corsi-base-spec/src/js/tippy.js
@@ -3,7 +3,7 @@
     content: (reference) => {
       const code = reference.getAttribute("data-tooltip");
 
-      return `<div class="w-[700px] bg-slate-600">
+      return `<div class="w-[50vw] bg-slate-600">
       <img src="img/table-${code.toLowerCase()}.png" /></div>`;
     },
     allowHTML: true,
